dbfcc590dec0ed76c77b23f108b4cb5a47e667c0
Volodymyr Viter <mad.tvido@gmail.com>
2021-06-08 20:35:19 +0300

    Update Feedback.js
    Додає прорахуно загальної кількості фідбеків та розраховує пересентейдж.

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,32 +1,61 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
 
 class Feedback extends Component {
+  static defaultProps = {
+    initialGood: 0,
+    initialNeutral: 0,
+    initialBad: 0,
+    // initialTotal: 0,
+    // initialPositivePercentage: 0,
+  };
+
+  static propTypes = {
+    good: PropTypes.number,
+    neutral: PropTypes.number,
+    bad: PropTypes.number,
+  };
+
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  }
+    good: this.props.initialGood,
+    neutral: this.props.initialNeutral,
+    bad: this.props.initialBad,
+    // total: this.props.initialTotal,
+    // positivePercentage: this.props.initialPositivePercentage,
+  };
 
   onClickGood = () => {
     this.setState(prevState => ({
       good: prevState.good + 1,
     }));
-  }
+  };
 
   onClickNeutral = () => {
     this.setState(prevState => ({
       neutral: prevState.neutral + 1,
     }));
-  }
+  };
 
   onClickBad = () => {
     this.setState(prevState => ({
       bad: prevState.bad + 1,
     }));
-  }
+  };
+
+  countTotalFeedback = () =>
+    this.state.good + this.state.bad + this.state.neutral;
+
+  countPositivePercentage = () => {
+    const total = this.countTotalFeedback();
+    return total
+      ? Math.floor((this.state.good / this.countTotalFeedback()) * 100)
+      : 0;
+  };
 
   render() {
+    const { good, neutral, bad } = this.state;
+
     return (
       <>
         <FeedbackOptions
@@ -34,14 +63,21 @@ class Feedback extends Component {
           onClickNeutral={this.onClickNeutral}
           onClickBad={this.onClickBad}
         />
+
+        {/* <Statistics good={} neutral={} bad={} total={} positivePercentage={}></Statistics> */}
+
         <div className="statistic__result">
           <h1 className="">Statistics</h1>
-          <span className="">Good: {this.state.good}</span>
-          <span className="">Neutral: {this.state.neutral}</span>
-          <span className="">Bad: {this.state.bad}</span>
+          <span className="">Good: {good}</span>
+          <span className="">Neutral: {neutral}</span>
+          <span className="">Bad: {bad}</span>
+          <span className="">Total: {this.countTotalFeedback()}</span>
+          <span className="">
+            Positive percentage: {this.countPositivePercentage()}%
+          </span>
         </div>
       </>
-    )
+    );
   }
 }
 
