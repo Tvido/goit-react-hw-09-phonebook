75b8ce9d6c67b6130186a60df78ddbc37894c6c1
Volodymyr Viter <mad.tvido@gmail.com>
2021-06-14 19:22:04 +0300

    Update ContactsList.js

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const ContactsList = ({ contacts, onDeleteContact }) => (
   <ul>
-    {contacts.map(({ id, name }) => (
+    {contacts.map(({ id, name, number }) => (
       <li key={id}>
         <p>{name}</p>
+        <p>{number}</p>
         <button onClick={() => onDeleteContact(id)}>Delete</button>
       </li>
     ))}
