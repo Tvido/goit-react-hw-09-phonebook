b360dde5a2e56c788e8e3c41c5e148097b771c14
Volodymyr Viter <mad.tvido@gmail.com>
2021-07-12 13:46:42 +0300

    Create contacts-types.js

---
+++ src/redux/contacts/contacts-types.js
@@ -0,0 +1,11 @@
+export default {
+  ADD: 'contacts/add',
+  DELETE: 'contacts/delete',
+  CHANGE_FILTER: 'contacts/changeFilter',
+};
+
+// const ADD = 'contacts/add';
+// const DELETE = 'contacts/delete';
+// const CHANGE_FILTER = 'contacts/changeFilter';
+
+// export default { ADD, DELETE, CHANGE_FILTER };
