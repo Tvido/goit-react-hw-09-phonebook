2cb63e9bab97ef2bac584eea4835d0dcc86606fb
Volodymyr Viter <mad.tvido@gmail.com>
2021-07-13 22:17:05 +0300

    delete all comments

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -14,23 +14,4 @@ const addContact = createAction('contacts/add', ({ name, number }) => {
 const deleteContact = createAction('contacts/delete');
 const changeFilter = createAction('contacts/changeFilter');
 
-// const addContact = ({ name, number }) => ({
-//   type: types.ADD,
-//   payload: {
-//     id: shortid.generate(),
-//     name,
-//     number,
-//   },
-// });
-
-// const deleteContact = ({ id }) => ({
-//   type: types.DELETE,
-//   payload: id,
-// });
-
-// const changeFilter = event => ({
-//   type: types.CHANGE_FILTER,
-//   payload: event,
-// });
-
 export default { addContact, deleteContact, changeFilter };
--- src/redux/contacts/contacts-reduser.js
+++ src/redux/contacts/contacts-reduser.js
@@ -12,27 +12,4 @@ const filter = createReducer('', {
   [actions.changeFilter]: (_, { payload }) => payload,
 });
 
-// const items = (state = [], { type, payload }) => {
-//   switch (type) {
-//     case types.ADD:
-//       return [payload, ...state];
-
-//     case types.DELETE:
-//       return state.filter(({ id }) => id !== payload);
-
-//     default:
-//       return state;
-//   }
-// };
-
-// const filter = (state = '', { type, payload }) => {
-//   switch (type) {
-//     case 'contacts/changeFilter':
-//       return payload;
-
-//     default:
-//       return state;
-//   }
-// };
-
 export default combineReducers({ items, filter });
