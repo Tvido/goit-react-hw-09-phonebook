6a23cc82efb337335f4f69ca1179da64f4aa4538
Volodymyr Viter <mad.tvido@gmail.com>
2021-07-13 20:13:01 +0300

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import contactsActions from '../../redux/contacts/contacts-actions';
 
 import './Filter.css';
 
@@ -15,4 +17,12 @@ Filter.propTypes = {
   onChange: PropTypes.func.isRequired,
 };
 
-export default Filter;
+const mapStateToProps = state => ({
+  value: state.contacts.filter,
+});
+
+const mapDaspatchToProps = dispatch => ({
+  onChange: event => dispatch(contactsActions.changeFilter(event.target.value)),
+});
+
+export default connect(mapStateToProps, mapDaspatchToProps)(Filter);
