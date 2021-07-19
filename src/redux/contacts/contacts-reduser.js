fa5030d966c1d546df2081dc0da2e8afeb2b68fc
Volodymyr Viter <mad.tvido@gmail.com>
2021-07-19 11:03:57 +0300

    Update contacts-reduser.js

--- src/redux/contacts/contacts-reduser.js
+++ src/redux/contacts/contacts-reduser.js
@@ -11,12 +11,6 @@ const items = createReducer([], {
     state.filter(({ id }) => id !== payload),
 });
 
-// const items = createReducer([], {
-//   [actions.addContact]: (state, { payload }) => [...state, payload],
-//   [actions.deleteContact]: (state, { payload }) =>
-//     state.filter(({ id }) => id !== payload),
-// });
-
 const filter = createReducer('', {
   [actions.changeFilter]: (_, { payload }) => payload,
 });
