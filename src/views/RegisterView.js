2588528798388d0c88642f3e74347f45fc03927e
Volodymyr Viter <mad.tvido@gmail.com>
2021-08-01 00:09:20 +0300

    Update RegisterView.js

--- src/views/RegisterView.js
+++ src/views/RegisterView.js
@@ -1,84 +1,159 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { authOperations } from '../redux/auth';
+import React, { useState } from 'react';
+// import { connect } from 'react-redux';
+// import { authOperations } from '../redux/auth';
 
 import './RegisterView.css';
 
-class RegisterView extends Component {
-  state = {
-    name: '',
-    email: '',
-    password: '',
+export default function RegisterView() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const updateName = event => {
+    setName(event.target.value);
   };
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
+  const updateEmail = event => {
+    setEmail(event.target.value);
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
-
-    this.props.onRegister(this.state);
-
-    this.setState({ name: '', email: '', password: '' });
+  const updatePassword = event => {
+    setPassword(event.target.value);
   };
 
-  render() {
-    const { name, email, password } = this.state;
+  const handleSubmit = event => {
+    event.preventDefault();
 
-    return (
-      <div>
-        <h1 className="loginView__title">Registration Page</h1>
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
 
-        <form
-          className="loginView__form"
-          onSubmit={this.handleSubmit}
-          autoComplete="off"
-        >
-          <label className="loginView__form__label">
-            Name:
-            <input
-              className="loginView__form__input"
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-            />
-          </label>
+  return (
+    <div>
+      <h1 className="loginView__title">Registration Page</h1>
 
-          <label className="loginView__form__label">
-            Email:
-            <input
-              className="loginView__form__input"
-              type="email"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-          </label>
+      <form
+        className="loginView__form"
+        onSubmit={handleSubmit}
+        autoComplete="off"
+      >
+        <label className="loginView__form__label">
+          Name:
+          <input
+            className="loginView__form__input"
+            type="text"
+            name="name"
+            value={name}
+            onChange={updateName}
+          />
+        </label>
 
-          <label className="loginView__form__label">
-            Password:
-            <input
-              className="loginView__form__input"
-              type="password"
-              name="password"
-              value={password}
-              onChange={this.handleChange}
-            />
-          </label>
+        <label className="loginView__form__label">
+          Email:
+          <input
+            className="loginView__form__input"
+            type="email"
+            name="email"
+            value={email}
+            onChange={updateEmail}
+          />
+        </label>
 
-          <button className="loginView__button" type="submit">
-            Register
-          </button>
-        </form>
-      </div>
-    );
-  }
+        <label className="loginView__form__label">
+          Password:
+          <input
+            className="loginView__form__input"
+            type="password"
+            name="password"
+            value={password}
+            onChange={updatePassword}
+          />
+        </label>
+
+        <button className="loginView__button" type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
 
-const mapDispatchToProps = {
-  onRegister: authOperations.register,
-};
+// class RegisterView extends Component {
+//   state = {
+//     name: '',
+//     email: '',
+//     password: '',
+//   };
 
-export default connect(null, mapDispatchToProps)(RegisterView);
+//   handleChange = ({ target: { name, value } }) => {
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     this.props.onRegister(this.state);
+
+//     this.setState({ name: '', email: '', password: '' });
+//   };
+
+//   render() {
+//     const { name, email, password } = this.state;
+
+//     return (
+//       <div>
+//         <h1 className="loginView__title">Registration Page</h1>
+
+//         <form
+//           className="loginView__form"
+//           onSubmit={this.handleSubmit}
+//           autoComplete="off"
+//         >
+//           <label className="loginView__form__label">
+//             Name:
+//             <input
+//               className="loginView__form__input"
+//               type="text"
+//               name="name"
+//               value={name}
+//               onChange={this.handleChange}
+//             />
+//           </label>
+
+//           <label className="loginView__form__label">
+//             Email:
+//             <input
+//               className="loginView__form__input"
+//               type="email"
+//               name="email"
+//               value={email}
+//               onChange={this.handleChange}
+//             />
+//           </label>
+
+//           <label className="loginView__form__label">
+//             Password:
+//             <input
+//               className="loginView__form__input"
+//               type="password"
+//               name="password"
+//               value={password}
+//               onChange={this.handleChange}
+//             />
+//           </label>
+
+//           <button className="loginView__button" type="submit">
+//             Register
+//           </button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapDispatchToProps = {
+//   onRegister: authOperations.register,
+// };
+
+// export default connect(null, mapDispatchToProps)(RegisterView);
