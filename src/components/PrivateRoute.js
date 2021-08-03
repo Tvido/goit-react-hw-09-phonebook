316c28a35c036af10e533e4ba5da0d9178aa5e79
Volodymyr Viter <mad.tvido@gmail.com>
2021-08-04 00:42:07 +0300

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
 import { authSelectors } from '../redux/auth';
+import { useSelector } from 'react-redux';
 
-const PrivateRoute = ({
+export default function PrivateRoute({
   component: Component,
   isAuthenticated,
   redirectTo,
   ...routeProps
-}) => (
-  <Route
-    {...routeProps}
-    render={props =>
-      isAuthenticated ? <Component {...props} /> : <Redirect to={redirectTo} />
-    }
-  />
-);
+}) {
+  const isLoggedIn = useSelector(authSelectors.getIsAuthenticated);
 
-const mapStateToProps = state => ({
-  isAuthenticated: authSelectors.getIsAuthenticated(state),
-});
+  return (
+    <Route
+      {...routeProps}
+      render={props =>
+        isLoggedIn ? <Component {...props} /> : <Redirect to={redirectTo} />
+      }
+    />
+  );
+}
+// const mapStateToProps = state => ({
+//   isAuthenticated: authSelectors.getIsAuthenticated(state),
+// });
 
-export default connect(mapStateToProps)(PrivateRoute);
+// export default connect(mapStateToProps)(PrivateRoute);
