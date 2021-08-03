981e99def2a8a94e3fbf5efac7e52b1ad4c9c5f6
Volodymyr Viter <mad.tvido@gmail.com>
2021-08-03 23:07:31 +0300

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
 import { filterContacts } from '../../redux/contacts';
 import { getFilter } from '../../redux/contacts/contacts-selectors';
 
 import './Filter.css';
 
-const Filter = ({ value, onChange }) => (
-  <div>
-    <label className="filter">
-      Find contacts by name
-      <input className="" type="text" value={value} onChange={onChange} />
-    </label>
-  </div>
-);
+export default function Filter() {
+  const dispatch = useDispatch();
+  const value = useSelector(getFilter);
+
+  const onChange = useCallback(
+    e => dispatch(filterContacts(e.target.value)),
+    [dispatch],
+  );
+
+  return (
+    <div>
+      <label className="filter">
+        Find contacts by name
+        <input className="" type="text" value={value} onChange={onChange} />
+      </label>
+    </div>
+  );
+}
 
 Filter.propTypes = {
   onChange: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({
-  value: getFilter(state),
-});
+// const Filter = ({ value, onChange }) => (
+//   <div>
+//     <label className="filter">
+//       Find contacts by name
+//       <input className="" type="text" value={value} onChange={onChange} />
+//     </label>
+//   </div>
+// );
 
-const mapDispatchToProps = dispatch => ({
-  onChange: e => dispatch(filterContacts(e.target.value)),
-});
+// Filter.propTypes = {
+//   onChange: PropTypes.func.isRequired,
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Filter);
+// const mapStateToProps = state => ({
+//   value: getFilter(state),
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   onChange: e => dispatch(filterContacts(e.target.value)),
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Filter);
