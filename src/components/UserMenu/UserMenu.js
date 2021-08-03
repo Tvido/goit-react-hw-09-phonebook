085dde777e9f5b9775e6e909dee1ff0eb3413389
Volodymyr Viter <mad.tvido@gmail.com>
2021-08-03 22:53:12 +0300

    Update UserMenu.js

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+// import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { authSelectors, authOperations } from '../../redux/auth';
 
 import './UserMenu.css';
 
-const UserMenu = ({ email, onLogout }) => (
-  <div className="userMenu__container">
-    <p className="userMenu__email">{email}</p>
-    <button className="userMenu__button" type="button" onClick={onLogout}>
-      Logout
-    </button>
-  </div>
-);
+export default function UserMenu() {
+  const dispatch = useDispatch();
 
-const mapStateToProps = state => ({
-  email: authSelectors.getUsermail(state),
-});
+  const email = useSelector(authSelectors.getUsermail);
+  // const onLogout = () => dispatch(authOperations.logOut());
 
-const mapDispatchToProps = {
-  onLogout: authOperations.logOut,
-};
+  const onLogout = useCallback(
+    () => dispatch(authOperations.logOut()),
+    [dispatch],
+  );
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserMenu);
+  return (
+    <div className="userMenu__container">
+      <p className="userMenu__email">{email}</p>
+      <button className="userMenu__button" type="button" onClick={onLogout}>
+        Logout
+      </button>
+    </div>
+  );
+}
+
+// const UserMenu = ({ email, onLogout }) => (
+//   <div className="userMenu__container">
+//     <p className="userMenu__email">{email}</p>
+//     <button className="userMenu__button" type="button" onClick={onLogout}>
+//       Logout
+//     </button>
+//   </div>
+// );
+
+// const mapStateToProps = state => ({
+//   email: authSelectors.getUsermail(state),
+// });
+
+// const mapDispatchToProps = {
+//   onLogout: authOperations.logOut,
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(UserMenu);
